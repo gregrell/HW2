f5d0bfb848cc4905bb42d948ec5e267f9bd82aeb
Greg Rell <gregory.rell@universalorlando.com>
2019-10-07 21:15:41 -0400

    Tokenization of docs working in rough fashion

--- segmenter.py
+++ segmenter.py
@@ -8,9 +8,20 @@ def perform():
     hockeyDirectory ="20news-bydate/20news-bydate/20news-bydate-train/rec.sport.hockey"
     autosDirectory ="20news-bydate/20news-bydate/20news-bydate-train/rec.autos"
 
+    #Grab file data into doc list objects
     hockeyDocs=readDocs(hockeyDirectory)
     autosDocs=readDocs(autosDirectory)
     print('training data read')
+
+    #Tokenize the documents
+
+
+
+
+    hockeyDocsAsTokens=docsAsTokens(hockeyDocs)
+    #autosDocsAsTokens=docsAsTokens(autosDocs)
+
+    print(hockeyDocsAsTokens[0])
 
 
 
@@ -22,4 +33,17 @@ def readDocs(path):
             docAsList.append(file.read())
             file.close()
     return docAsList
+
+def docsAsTokens(docs):
+    nlp = spacy.load("en_core_web_sm")
+    docAsToken=[]
+    for doc in docs:
+        tokens=[]
+        spacy_doc=nlp(doc)
+        for token in spacy_doc:
+            #print(token.text)
+            tokens.append(token)
+        docAsToken.append(tokens)
+    return docAsToken
+
 
