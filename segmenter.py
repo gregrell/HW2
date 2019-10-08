29ba35991fdeac2c54a5b65e34e5502e7bea5b35
Greg Rell <gregory.rell@universalorlando.com>
2019-10-08 16:51:16 -0400

    File save/read implemented to save processing time during development

--- segmenter.py
+++ segmenter.py
@@ -1,7 +1,7 @@
 import spacy
 import os
 from collections import Counter
-
+import pickle
 
 
 
@@ -12,25 +12,59 @@ def perform():
     print("Performing Segmentation")
     hockeyDirectory ="20news-bydate/20news-bydate-train/rec.sport.hockey"
     autosDirectory ="20news-bydate/20news-bydate-train/rec.autos"
+    hockeyTestDirectory="20news-bydate/20news-bydate-test/rec.sport.hockey"
+    autosTestDirectory ="20news-bydate/20news-bydate-test/rec.autos"
+    pathNlpHockeyDocs="nlpHockeyDocs.txt"
+    pathNlpAutosDocs="nlpAutosDocs.txt"
+    pathNlpHockeyTestDocs="nlpHockeyTestDocs.txt"
+    pathNlpAutosTestDocs="nlpAutosTestDocs.txt"
+
 
     #Grab file data into doc list objects
     hockeyDocs=readDocs(hockeyDirectory)
     autosDocs=readDocs(autosDirectory)
+    hockeyTestDocs=readDocs(hockeyTestDirectory)
+    autosTestDocs=readDocs(autosTestDirectory)
     print('training data read')
 
-    #nlp the documents
-    nlpHockeyDocs=nlpDocs(hockeyDocs)
-    nlpAutosDocs=nlpDocs(autosDocs)
 
-    print('converted to NLP data')
+    #load/save nlp docs to aide in speed of development
+    nlpHockeyDocs,read=openNlpDoc(pathNlpHockeyDocs)
+    if not read:
+        print('Could not find saved data... performing nlp conversion on Hockey Docs')
+        nlpHockeyDocs = nlpDocs(hockeyDocs)
+        saveNlpDoc(pathNlpHockeyDocs,nlpHockeyDocs)
+
+    nlpAutosDocs,read=openNlpDoc(pathNlpAutosDocs)
+    if not read:
+        print('Could not find saved data... performing nlp conversion on Autos Docs')
+        nlpAutosDocs = nlpDocs(autosDocs)
+        saveNlpDoc(pathNlpAutosDocs,nlpAutosDocs)
+
+    nlpHockeyTestDocs,read=openNlpDoc(pathNlpHockeyTestDocs)
+    if not read:
+        print('Could not find saved data... performing nlp conversion on Hockey Testing Docs')
+        nlpHockeyTestDocs = nlpDocs(hockeyTestDocs)
+        saveNlpDoc(pathNlpHockeyTestDocs,nlpHockeyTestDocs)
+
+
+    nlpAutosTestDocs,read=openNlpDoc(pathNlpAutosTestDocs)
+    if not read:
+        print('Could not find saved data... performing nlp conversion on Autos Testing Docs')
+        nlpAutosTestDocs = nlpDocs(autosTestDocs)
+        saveNlpDoc(pathNlpAutosTestDocs,nlpAutosTestDocs)
+
     print('Total Hockey Docs ',len(nlpHockeyDocs))
     print('Total Autos Docs', len(nlpAutosDocs))
+    print('Total Hockey TESTING Docs ',len(nlpHockeyTestDocs))
+    print('Total Autos TESTING Docs', len(nlpAutosTestDocs))
+
 
     #tokenize
     tokenizedHockeyDocs = tokenize(nlpHockeyDocs)
     tokenizedAutosDocs = tokenize(nlpAutosDocs)
 
-    print('Tokenized')
+    print('Tokenized Training Docs')
 
     #group into bag of unique words returned as Counter objects
     dictHockeyWords,bagofHockeyWords=bagOfWords(tokenizedHockeyDocs)
@@ -79,6 +113,27 @@ def bagOfWords(docs):
         bagofWords.extend(doc)
     cnt = Counter(bagofWords)
     return cnt, bagofWords
+
+def saveNlpDoc(path,docs):
+    fb = open(path, 'wb')
+    pickle.dump(docs, fb)
+    print('Saved data to ',path)
+    fb.close()
+
+def openNlpDoc(path):
+    itemlist=[]
+    if os.path.exists(path):
+        fb = open(path, 'rb')
+        itemlist=pickle.load(fb)
+        print('Successfully read data from ',path)
+        return itemlist,True
+    else:
+        print('Could not find ',path)
+        return itemlist,False
+
+
+
+
 
 
 
