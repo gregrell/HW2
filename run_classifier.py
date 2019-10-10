dd96bef58eecf272e3ad7fb8f0dca1e508a3b36f
Greg Rell <gregory.rell@universalorlando.com>
2019-10-10 12:34:28 -0400

    Removed non essential files

--- run_classifier.py
+++ run_classifier.py
@@ -1,26 +1,9 @@
 
-from sentiment_reader import SentimentCorpus
-from multinomial_naive_bayes import MultinomialNaiveBayes
 import HW2
-import sklearn_stuff
 
 if __name__ == '__main__':
 
-    dataset = SentimentCorpus()
-    nb = MultinomialNaiveBayes()
     HW2.Run()
-    #sklearn_stuff.Run()
-    
-    #params = nb.train(dataset.train_X, dataset.train_y)
-
-
-    #predict_train = nb.test(dataset.train_X, params)
-    #eval_train = nb.evaluate(predict_train, dataset.train_y)
-    
-    #predict_test = nb.test(dataset.test_X, params)
-    #eval_test = nb.evaluate(predict_test, dataset.test_y)
-    
-    #print("Accuracy on training set: %f, on test set: %f" % (eval_train, eval_test))
 
 
 
